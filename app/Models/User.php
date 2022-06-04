0e75452fbb69d3176beb98a749b7fd574dfebf1e
Kaka <kakalition@gmail.com>
2022-06-04 16:51:43 +0700

    Add authorization_code column to User migration; Create User seeder to populate User database with Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'authorization_code',
     ];
 
     /**
@@ -31,6 +32,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'authorization_code',
     ];
 
     /**
