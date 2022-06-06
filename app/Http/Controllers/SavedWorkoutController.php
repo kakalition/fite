ef17c9174fe1b4fabe7df07d126de64ae6824681
Kaka <kakalition@gmail.com>
2022-06-06 12:24:42 +0700

    Create SavedWorkoutValidator

--- app/Http/Controllers/SavedWorkoutController.php
+++ app/Http/Controllers/SavedWorkoutController.php
@@ -3,40 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SavedWorkoutResource;
-use App\Models\Exercise;
 use App\Models\SavedWorkout;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Validator\SavedWorkoutValidator;
 
 class SavedWorkoutController extends Controller
 {
 
-  private function exercise_constraints_checker($type, $reps, $weights_in_kg, $durations_in_sec)
-  {
-    if ($type === 0 && $reps === null) {
-      return response('Bodyweight training should include repetitions.', 400);
-    }
-
-    if ($type === 1 && ($reps === null || $weights_in_kg === null)) {
-      return response('Weight training should include repetitions and weights.', 400);
-    }
-
-    if ($type === 2 && $durations_in_sec === null) {
-      return response('Interval training should include durations.', 400);
-    }
-
-    if ($type < 0 || $type > 2) {
-      return response('Exercise type not recognized.', 400);
-    }
-
-    return null;
-  }
-
   public function index()
   {
     $saved_workouts = SavedWorkoutResource::collection(SavedWorkout::all());
-    return response($saved_workouts);
+    return response(SavedWorkout::all());
   }
 
   /* Exercises JSON Format
@@ -48,44 +26,27 @@ class SavedWorkoutController extends Controller
    */
   public function store(Request $request)
   {
-    $title = $request->input('title');
-    $user_id = $request->input('user_id');
+    $validator = new SavedWorkoutValidator();
+    $validation_status = $validator->validate($request);
+
+    if ($validation_status != null) {
+      return $validation_status;
+    }
+
     $public_workout_id = $request->input('public_workout_id');
-    $exercises = $request->input('exercises');
-
     $model = null;
-
-    if ($public_workout_id != null && $exercises != null) {
-      return response('Public workout id and exercises cannot exist at the same time.', 400);
-    }
-
-    foreach ($exercises as $exercise) {
-      $_exercise = Exercise::where('id', $exercise['exercise_id'])
-        ->first();
-
-      $constraint_status = $this->exercise_constraints_checker(
-        $_exercise->type,
-        $exercise['reps'] ?? null,
-        $exercise['weights_in_kg'] ?? null,
-        $exercise['duration_in_sec'] ?? null,
-      );
-
-      if ($constraint_status != null) {
-        return $constraint_status;
-      }
-    }
 
     if ($public_workout_id != null) {
       $model = SavedWorkout::create([
-        'user_id' => $user_id,
-        'title' => $title,
+        'user_id' => $request->input('user_id'),
+        'title' => $request->input('title'),
         //'public_workout_id' => $public_workout_id,
       ]);
     } else {
       $model = SavedWorkout::create([
-        'user_id' => $user_id,
-        'title' => $title,
-        'exercises' => json_encode($exercises),
+        'user_id' => $request->input('user_id'),
+        'title' => $request->input('title'),
+        'exercises' => json_encode($request->input('exercises')),
       ]);
     }
 
---
+++ app/Validator/SavedWorkoutValidator.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Validator;
+
+use App\Models\Exercise;
+use Illuminate\Http\Request;
+
+class SavedWorkoutValidator
+{
+
+  private function validate_exercise($exercise)
+  {
+    $is_reps_available = isset($exercise['reps']);
+    $is_weights_available = isset($exercise['weights_in_kg']);
+    $is_duration_available = isset($exercise['duration_in_sec']);
+
+    $exercise_type = Exercise::where('id', $exercise['exercise_id'])
+      ->first()
+      ->type;
+
+    if (
+      $exercise_type === 0
+      && $is_reps_available ? $exercise['reps'] === null : true
+    ) {
+      return response('Bodyweight training should include repetitions.', 400);
+    }
+
+    if (
+      $exercise_type === 1
+      && ($is_reps_available ? $exercise['reps'] === null : true)
+      || ($is_weights_available ? $exercise['weights_in_kg'] === null : true)
+    ) {
+      return response('Weight training should include repetitions and weights.', 400);
+    }
+
+    if (
+      $exercise_type === 2
+      && $is_duration_available ? $exercise['durations_in_sec'] == null : true
+    ) {
+      return response('Interval training should include durations.', 400);
+    }
+
+    if (
+      $exercise_type < 0
+      || $exercise_type > 2
+    ) {
+      return response('Exercise type not recognized.', 400);
+    }
+  }
+
+  private function validate_exclusivity($public_workout_id, $exercises)
+  {
+    if (
+      $public_workout_id != null
+      && $exercises != null
+    ) {
+      return response('Public workout id and exercises cannot exist at the same time.', 400);
+    }
+
+    return null;
+  }
+
+  public function validate(Request $request)
+  {
+    $exclusivity_validation = $this->validate_exclusivity(
+      $request->input('public_workout_id'),
+      $request->input('exercises'),
+    );
+
+    if ($exclusivity_validation != null) {
+      return $exclusivity_validation;
+    }
+
+    $exercise_validation = null;
+    foreach ($request->input('exercises') as $exercise) {
+      $exercise_validation = $this->validate_exercise($exercise);
+      if ($exercise_validation != null) break;
+    }
+
+    if ($exercise_validation != null) {
+      return $exercise_validation;
+    }
+
+    return null;
+  }
+}
