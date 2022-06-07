cff3abb2adeb7952155885ce1e2bbddbfd592c40
Kaka <kakalition@gmail.com>
2022-06-07 11:03:33 +0700

    Create PublicWorkout migration

---
+++ app/Http/Controllers/PublicWorkoutController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PublicWorkout;
+use Illuminate\Http\Request;
+
+class PublicWorkoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PublicWorkout  $publicWorkout
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PublicWorkout $publicWorkout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PublicWorkout  $publicWorkout
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PublicWorkout $publicWorkout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PublicWorkout  $publicWorkout
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PublicWorkout $publicWorkout)
+    {
+        //
+    }
+}
