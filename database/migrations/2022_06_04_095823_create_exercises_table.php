1f6e2a850012ca1d75b840ee4c97b3bf72d42081
Kaka <kakalition@gmail.com>
2022-06-08 08:06:50 +0700

    Add user_id foreign key column to Exercise migration

--- database/migrations/2022_06_04_095823_create_exercises_table.php
+++ database/migrations/2022_06_04_095823_create_exercises_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
   {
     Schema::create('exercises', function (Blueprint $table) {
       $table->id();
+      $table->foreignId('user_id')
+        ->references('id')
+        ->on('users')
+        ->cascadeOnDelete();
       $table->string('title');
       $table->integer('type');
       $table->timestamps();
